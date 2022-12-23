8bda827a6638e233df7d9565fe1447646c88e4c0
dangen <dangen.effy@gmail.com>
2022-12-23 14:12:20 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 /opt/homebrew/bin/brew install just
+
+echo 'export PATH="/opt/homebrew/bin:$PATH"' >> ~/.zshrc
