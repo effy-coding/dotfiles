a02bf650f1f75d60397a766be7b60e9dc02f5ff9
nobody <nobody@gmail.com>
2026-02-03 17:51:28 +0900

    fix: nvim Cmd+P/Cmd+Shift+F keybindings in tmux
    Use Ctrl+] escape sequence instead of unbind to pass Cmd keys
    through tmux to nvim. Also add extended-keys support to tmux.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,9 +6,15 @@
 local map = vim.keymap.set
 
 -- Cmd+P: File search (like VSCode's Quick Open)
+-- Ghostty sends Ctrl+] p (\x1dp) for Cmd+P via tmux
 map("n", "<D-p>", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
 map("i", "<D-p>", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+map("n", "<C-]>p", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+map("i", "<C-]>p", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
 
 -- Cmd+Shift+F: Global text search (like VSCode's Search)
+-- Ghostty sends Ctrl+] f (\x1df) for Cmd+Shift+F via tmux
 map("n", "<D-S-f>", "<cmd>Telescope live_grep<cr>", { desc = "Search in Files" })
 map("i", "<D-S-f>", "<cmd>Telescope live_grep<cr>", { desc = "Search in Files" })
+map("n", "<C-]>f", "<cmd>Telescope live_grep<cr>", { desc = "Search in Files" })
+map("i", "<C-]>f", "<cmd>Telescope live_grep<cr>", { desc = "Search in Files" })
